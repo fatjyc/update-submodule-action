06d7050aaacafbacc37bb8f971bc902b5c6f38af
Jiang Yucheng <fatjyc@gmail.com>
2023-05-25 14:41:12 +0800

    feat: finish

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,12 +46,6 @@ if [[ "$commit_msg" == *"Merge pull request"* ]]; then
 
   echo "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR}"
 
-  PR_INFO=$(curl -L \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer ${INPUT_TOKEN}"\
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR})
-
   user=$(curl -L \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${INPUT_TOKEN}"\
